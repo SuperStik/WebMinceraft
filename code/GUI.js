96922074c9734c31d505697380dda2b7a0a85129
SuperStik <daviswood290@gmail.com>
2022-05-24 15:32:14 -0700

    Add progress bar and change styling

--- code/GUI.js
+++ code/GUI.js
@@ -1,42 +1,45 @@
 class GUI {
-    static setGenInfo() {
-        document.getElementById("main_menu").hidden = true;
-        document.getElementById("gen_info").hidden = false;
-        document.getElementById("selector").hidden = true;
-        document.getElementById("glCanvas").hidden = true;
-    }
+	static gen_info_box = document.getElementById("gen_info_box");
+	static gen_info = document.getElementById("gen_info");
+	static gen_info_progress = document.getElementById("gen_info_progress");
+	static setGenInfo() {
+		document.getElementById("main_menu").hidden = true;
+		gen_info_box.hidden = false;
+		document.getElementById("selector").hidden = true;
+		document.getElementById("glCanvas").hidden = true;
+	}
 
-    static genInfoShowText(text) {
-        var gen_info = document.getElementById("gen_info");
-        gen_info.innerText = text;
-    }
+	static genInfoShowText(text, amount) {
+		gen_info.innerText = text;
+		gen_info_progress.value = amount;
+	}
 
-    static setPlaying(click) {
-        document.getElementById("main_menu").hidden = true;
-        document.getElementById("gen_info").hidden = true;
+	static setPlaying(click) {
+		document.getElementById("main_menu").hidden = true;
+		gen_info_box.hidden = true;
 
-        document.getElementById("glCanvas").hidden = false;
-        document.getElementById("selector").hidden = false;
-        const scale = 4;
-        const border = 4;
-        var selector = document.getElementById("selector");
-        selector.hidden = false;
-        for(var i = 0; i < global.selector.length; i++) {
-            var material = document.createElement("div");
-            material.block = world.blockMaps.get(global.selector[i]);
-            const coords = material.block.sides;
-            material.style.backgroundPosition = (-coords[0] * 16 * scale) + "px " + (-coords[1] * 16 * scale) + "px";
-            material.style.backgroundSize = 256 * scale + "px " + 256 * scale + "px";
-            material.style.height = 16 * scale + "px";
-            material.style.width = 16 * scale + "px";
-    
-            material.onclick = function() {
-                click(this.block.name);
-            };
-            
-            selector.appendChild(material);
-        }
-        selector.style.padding = border + "px";
-        selector.style.marginLeft = (border + Math.floor(-global.selector.length * 16 * scale / 2)) + "px";
-    }
+		document.getElementById("glCanvas").hidden = false;
+		document.getElementById("selector").hidden = false;
+		const scale = 4;
+		const border = 4;
+		var selector = document.getElementById("selector");
+		selector.hidden = false;
+		for(var i = 0; i < global.selector.length; i++) {
+			var material = document.createElement("div");
+			material.block = world.blockMaps.get(global.selector[i]);
+			const coords = material.block.sides;
+			material.style.backgroundPosition = (-coords[0] * 16 * scale) + "px " + (-coords[1] * 16 * scale) + "px";
+			material.style.backgroundSize = 256 * scale + "px " + 256 * scale + "px";
+			material.style.height = 16 * scale + "px";
+			material.style.width = 16 * scale + "px";
+	
+			material.onclick = function() {
+				click(this.block.name);
+			};
+			
+			selector.appendChild(material);
+		}
+		selector.style.padding = border + "px";
+		selector.style.marginLeft = (border + Math.floor(-global.selector.length * 16 * scale / 2)) + "px";
+	}
 }
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var global = {
-    selector: ["dirt", "stone", "glass", "sand", "wood", "leaf", "bricks", "planks", "water"],
+	selector: ["dirt", "stone", "glass", "sand", "wood", "leaf", "bricks", "planks", "water"],
 }
 
 var rm = null;
@@ -7,44 +7,44 @@ var world = null;
 var input = null;
 
 function main() {
-    GUI.setGenInfo();
-    var render = new Renderer();
-    const maxFPS = 60;
-    input = new Input();
+	GUI.setGenInfo();
+	var render = new Renderer();
+	const maxFPS = 60;
+	input = new Input();
 
-    const size = parseInt(document.getElementById("world_size").value);
-    world = new World(size, 8, render);
+	const size = parseInt(document.getElementById("world_size").value);
+	world = new World(size, 8, render);
 
-    world.generateTerrain(function(info) {
-         GUI.genInfoShowText(info +
-            "\nProgres: " + Math.floor(world.progress * 100) + "%.");
-    }, function() {
-        GUI.setPlaying(function(block) {
-            world.player.buildBlock = block;
-        });
-        world.autoLoadChunks();
-        var last = Date.now();
+	world.generateTerrain(function(info) {
+		 GUI.genInfoShowText(info +
+			"\nProgress: " + Math.floor(world.progress * 100) + "%.", world.progress);
+	}, function() {
+		GUI.setPlaying(function(block) {
+			world.player.buildBlock = block;
+		});
+		world.autoLoadChunks();
+		var last = Date.now();
 
-        var imp = 0;
-        render.clearColor(0.53, 0.807, 0.98);
-        function draw() {
-            var delta = (Date.now() - last) / 1000;
-            last = Date.now();
-            input.update();
+		var imp = 0;
+		render.clearColor(0.53, 0.807, 0.98);
+		function draw() {
+			var delta = (Date.now() - last) / 1000;
+			last = Date.now();
+			input.update();
 
-            world.update(delta);
+			world.update(delta);
 
-            render.render();
+			render.render();
 
-            imp++;
-            if (imp >= 20) {
-                imp = 0;
-            }
+			imp++;
+			if (imp >= 20) {
+				imp = 0;
+			}
 
-            while(Date.now() - last < 1000 / maxFPS);
-            requestAnimationFrame(draw);
-        }
+			while(Date.now() - last < 1000 / maxFPS);
+			requestAnimationFrame(draw);
+		}
 
-        requestAnimationFrame(draw);
-    });
+		requestAnimationFrame(draw);
+	});
 }
